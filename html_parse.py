781f6ed08b809e3e5e506b5a1674d237c1127b68
cel243 <cel243@cornell.edu>
2021-01-17 11:37:33 -0500

    Create skeleton python file for html parsing, containing top level funtion definitions as detailed in the README

---
+++ html_parse.py
@@ -0,0 +1,21 @@
+""" This file contains the functionality for parsing HTML. """
+
+def extract_course_links(html):
+    """Parse html and return dictionary of {period_no: course link}."""
+    pass
+
+def extract_assignmentIDs(html):
+    """Parse html and return dictionary of {assignment_name: ID}."""
+    pass
+
+def extract_assignment_links(html):
+    """Calls extract_assignmentIDs and converts IDs to assignment result links. Returns dict of {assignment_name: link}."""
+    pass
+
+def extract_student_links(html):
+    """Parse html and return dictionary of {student_name: link}."""
+    pass
+
+def compute_student_score(html):
+    """Given student restuls page, return the total number of assignment problems that have a nonzero score."""
+    pass
